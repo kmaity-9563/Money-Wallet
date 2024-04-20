a5c1a5bef701b478a9b500c5f6523c83c92d0b62
koushikFusion <koushik@fusiontechlab.com>
2024-04-20 15:16:43 +0530

    erroe

--- apps/user/app/api/auth/[...nextauth]/route.ts
+++ apps/user/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import  authOptions  from '../../../lib/auth'
 // import  CredentialsProvider  from "next-auth/providers/credentials";
 
-const handler = NextAuth(authOptions)
+// const handler = NextAuth(authOptions)
 
 
 export { handler as GET, handler as POST }
