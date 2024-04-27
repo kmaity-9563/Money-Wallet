a4caa5bb757d583c118b259520d61ccd2d78cb17
koushikFusion <koushik@fusiontechlab.com>
2024-04-27 15:34:06 +0530

    final comit

---
+++ packages/Prisma/prisma/migrations/20240419073640_/migration.sql
@@ -0,0 +1,66 @@
+-- CreateEnum
+CREATE TYPE "onRampStatus" AS ENUM ('success', 'failure', 'processing');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "password" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "p2ptransfer" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "from" INTEGER NOT NULL,
+    "to" INTEGER NOT NULL,
+
+    CONSTRAINT "p2ptransfer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "onRampTransaction" (
+    "id" SERIAL NOT NULL,
+    "status" "onRampStatus" NOT NULL,
+    "token" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "onRampTransaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Balance" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "locked" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "onRampTransaction_token_key" ON "onRampTransaction"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Balance_userId_key" ON "Balance"("userId");
+
+-- AddForeignKey
+ALTER TABLE "p2ptransfer" ADD CONSTRAINT "p2ptransfer_from_fkey" FOREIGN KEY ("from") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "p2ptransfer" ADD CONSTRAINT "p2ptransfer_to_fkey" FOREIGN KEY ("to") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "onRampTransaction" ADD CONSTRAINT "onRampTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
